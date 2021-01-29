f2c39588cbb2ed3ca2893ce0d3dd6584764a128f
Tai4khum <taikhum@cronycle.com>
2021-01-29 13:05:36 +0530

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -43,7 +43,7 @@ define( 'DB_COLLATE', '' );
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
  * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
+ *   
  * @since 2.6.0
  */
 define( 'AUTH_KEY',          'HY:{VYFJD!5wWRd7Zl9vl}bI R>v3Ar9Ia0[|N$$=ddYF3V{91m8-WdlQO_%Pt5^' );
